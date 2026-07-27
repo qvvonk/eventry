afe82637cbb575cab4364bc47f0b7f4e0207ab04
qvvonk <qvvonk@gmail.com>
2026-07-27 17:37:58 +0300

    small changes

--- tests/eventry_asyncio_tests/event_life_test.py
+++ tests/eventry_asyncio_tests/event_life_test.py
@@ -10,46 +10,54 @@ router = DefaultRouter(name='test_router')
 async def router_outer_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'] = ['router.outer']
     await next_call(data)
+    event['workflow'].append('router.outer.end')
 
 
 @router.inner_middleware()
 async def router_inner_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'].append('router.inner')
     await next_call(data)
+    event['workflow'].append('router.inner.end')
 
 
 @router.on_event.manager_outer_middleware()
 async def manager_outer_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'].append('manager.outer')
     await next_call(data)
+    event['workflow'].append('manager.outer.end')
 
 
 @router.on_event.manager_inner_middleware()
 async def manager_inner_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'].append('manager.inner')
     await next_call(data)
+    event['workflow'].append('manager.inner.end')
 
 
 @router.on_event.handler_outer_middleware()
 async def handler_outer_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'].append('handler.outer')
     await next_call(data)
+    event['workflow'].append('handler.outer.end')
 
 
 @router.on_event.handler_inner_middleware()
 async def handler_inner_middleware(next_call, data, event: ExtendedEvent):
     event['workflow'].append('handler.inner')
     await next_call(data)
+    event['workflow'].append('handler.inner.end')
 
 
 async def handler_specific_outer_middleware(next_call, data, event: ExtendedEvent):
-    event['workflow'].append('handler_scpecific.outer')
+    event['workflow'].append('handler_specific.outer')
     await next_call(data)
+    event['workflow'].append('handler_specific.outer.end')
 
 
 async def handler_specific_inner_middleware(next_call, data, event: ExtendedEvent):
-    event['workflow'].append('handler_scpecific.inner')
+    event['workflow'].append('handler_specific.inner')
     await next_call(data)
+    event['workflow'].append('handler_specific.inner.end')
 
 
 @router.on_event(
@@ -69,11 +77,25 @@ async def test_event_workflow():
     assert event['workflow'] == [
         'router.outer',
         'router.inner',
+        #
         'manager.outer',
         'manager.inner',
+        #
         'handler.outer',
-        'handler_scpecific.outer',
+        'handler_specific.outer',
         'handler.inner',
-        'handler_scpecific.inner',
-        'handler'
+        'handler_specific.inner',
+        #
+        'handler',
+        #
+        'handler_specific.inner.end',
+        'handler.inner.end',
+        'handler_specific.outer.end',
+        'handler.outer.end',
+        #
+        'manager.inner.end',
+        'manager.outer.end',
+        #
+        'router.inner.end',
+        'router.outer.end'
     ]
